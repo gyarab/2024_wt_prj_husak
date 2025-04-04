5aef8440af75dfa4934a0588dcc3c91bb5169477
illia.husak.s@gyarab.cz <illia.husak.s@gyarab.cz>
2025-04-04 12:16:45 +0200

    something

--- prj/main/views.py
+++ prj/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Album
 
-# Create your views here.
+def album_list(request):
+    albums = Album.objects.all()
+    return render(request, 'templates/alba.html', {'albums': albums})
+
+
+
